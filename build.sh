b315c3c31302c6ddfab503cb4b287db92f3582a7
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-05-10 06:34:50 +0700

    fix: wrong dir

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ curl https://raw.githubusercontent.com/kennethreitz/pipenv/master/get-pipenv.py 
 pipenv install
 
 # Build our theme assets
-cd theme
+cd theme/mkdocs-material-custom
 
 yarn
 yarn build
 
-cd ..
+cd ../..
 
 # Build our docs
 pipenv run mkdocs build -f mkdocs.yml
